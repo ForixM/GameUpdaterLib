8c75de7d3298d4787015c7410614233fc4329213
forix <forixmehdi@gmail.com>
2020-03-21 16:08:36 +0100

    Fixed some bugs related to PO3 normal

--- src/ma/forix/gameupdater/Downloader.java
+++ src/ma/forix/gameupdater/Downloader.java
@@ -12,6 +12,8 @@ import java.net.Socket;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.jar.JarFile;
 
 public class Downloader extends Task<Void> {
 
@@ -21,10 +23,60 @@ public class Downloader extends Task<Void> {
     private JSONObject object;
     private int downloadSize, bytesDownloaded, fileDownloaded, filesToDownload, threadsNumber;
     private final int SIMULTANEOUS = 20;
-    private String[] ignoreFiles;
+    private ArrayList<String> ignoreFiles, ignoreModId;
     private BufferedInputStream reader;
     private Thread updateBar;
     private Os os;
+
+    public Downloader(String url, File gameDir){
+        System.out.println("OS: "+System.getProperty("os.name"));
+        if (System.getProperty("os.name").contains("Windows"))
+            os = Os.WINDAUBE;
+        else
+            os = Os.UNIX;
+
+        if (url.toCharArray()[url.toCharArray().length-1] == '/'){
+            this.url = UrlAdapter(url);
+        }
+        this.gameDir = gameDir;
+        if (!gameDir.exists())
+            gameDir.mkdir();
+        getContent();
+        getIgnoreList();
+    }
+
+    private String getModId(File file){
+        try {
+            byte[] buffer = new byte[1 << 14];
+            JarFile in = new JarFile(file);
+            try {
+                InputStream ein = in.getInputStream(in.getEntry("mcmod.info"));
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int nr; 0 < (nr = ein.read(buffer)); ) {
+                    stringBuilder.append(new String(buffer, 0, nr));
+                }
+                in.close();
+                StringBuilder builder = new StringBuilder();
+                for (String current : stringBuilder.toString().split("\n")){
+                    if (current.contains("\"modid\"")){
+                        builder = new StringBuilder(current);
+                        for (char currentC : builder.toString().toCharArray()) {
+                            if ((byte) currentC == 32 || (byte) currentC == 9 || (byte) currentC == 34 || (byte) currentC == 44) {
+                                builder.deleteCharAt(builder.indexOf(String.valueOf(currentC)));
+                            }
+                        }
+                    }
+                }
+                return builder.toString();
+            } catch (NullPointerException e){
+                e.printStackTrace();
+                return null;
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     private void getContent(){
         try {
@@ -52,36 +104,13 @@ public class Downloader extends Task<Void> {
         }
     }
 
-    private String read() throws IOException{
-        String response = "";
-        int stream;
-        byte[] b = new byte[1024];
-        stream = reader.read();
-        response = new String(b, 0, stream);
-        return response;
-    }
-
-    public Downloader(String url, File gameDir){
-        System.out.println("OS: "+System.getProperty("os.name"));
-        if (System.getProperty("os.name").contains("Windows"))
-            os = Os.WINDAUBE;
-        else
-            os = Os.UNIX;
-
-        if (url.toCharArray()[url.toCharArray().length-1] == '/'){
-            this.url = UrlAdapter(url);
-        }
-        this.gameDir = gameDir;
-        if (!gameDir.exists())
-            gameDir.mkdir();
-        getContent();
-
-
+    private void getIgnoreList(){
         try(InputStreamReader streamReader = new InputStreamReader(new URL(this.url+"/ignore.txt").openStream())){
             int data = streamReader.read();
             StringBuilder reading = new StringBuilder();
             boolean writing = true;
 
+            //Get ignore.txt content
             while (data != -1){
                 if (writing) {
                     if (data == 13)
@@ -92,19 +121,49 @@ public class Downloader extends Task<Void> {
                 data = streamReader.read();
             }
 
+            //Splitting ignore.txt content in a string list
+            String buffer[];
             if (reading.toString().contains("\r"))
-                ignoreFiles = reading.toString().split("\r");
+                buffer = reading.toString().split("\r");
             else
-                ignoreFiles = reading.toString().split("\n");
+                buffer = reading.toString().split("\n");
 
+            //Separating modid from ignored folders/files
+            ignoreModId = new ArrayList<>();
+            ignoreFiles = new ArrayList<>();
+            for (String current : buffer){
+                if (current.contains("modid:")){
+                    ignoreModId.add(current/*new StringBuilder(current).delete(0, 6).toString()*/);
+                } else {
+                    ignoreFiles.add(current);
+                }
+            }
+
+            //Display ignoreFiles content
             System.out.print("[");
             for (String current : ignoreFiles){
+                System.out.print("\""+current+"\", ");
+            }
+            System.out.println("]");
+
+            //Display ignoreModId content
+            System.out.print("[");
+            for (String current : ignoreModId){
                 System.out.print("\""+current+"\", ");
             }
             System.out.println("]");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String read() throws IOException{
+        String response = "";
+        int stream;
+        byte[] b = new byte[1024];
+        stream = reader.read();
+        response = new String(b, 0, stream);
+        return response;
     }
 
     private String UrlAdapter(String url){
@@ -115,7 +174,49 @@ public class Downloader extends Task<Void> {
         return sb.toString();
     }
 
+    private void vSync(){
+        File optionsFile = new File(gameDir.toString()+"\\options.txt");
+        StringBuilder optionsBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(optionsFile))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                optionsBuilder.append(line + "\n");
+            }
 
+            if (optionsBuilder.toString().contains("enableVsync:true")){
+                optionsBuilder.replace(optionsBuilder.indexOf("enableVsync")+12, optionsBuilder.indexOf("enableVsync")+16, "false");
+                optionsFile.delete();
+                FileWriter fw = new FileWriter(optionsFile);
+                fw.write(optionsBuilder.toString());
+                fw.close();
+                System.out.println("Disabled VSync");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fullScreen(){
+        File optionsFile = new File(gameDir.toString()+"\\options.txt");
+        StringBuilder optionsBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(optionsFile))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                optionsBuilder.append(line + "\n");
+            }
+
+            if (optionsBuilder.toString().contains("fullscreen:true")){
+                optionsBuilder.replace(optionsBuilder.indexOf("fullscreen")+11, optionsBuilder.indexOf("fullscreen")+15, "false");
+                optionsFile.delete();
+                FileWriter fw = new FileWriter(optionsFile);
+                fw.write(optionsBuilder.toString());
+                fw.close();
+                System.out.println("Disabled Fullscreen");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int GetDownloadSize(JSONArray toDownload){
         File cursor;
@@ -146,8 +247,20 @@ public class Downloader extends Task<Void> {
                             ignore = true;
                     }
                 }
+
             } catch (NoSuchAlgorithmException | IOException e) {
                 e.printStackTrace();
+            }
+            if (current.getParent().equals(System.getProperty("user.home")+"\\AppData\\Roaming\\.modcraft\\mods")){
+                for (String modid : ignoreModId){
+                    try {
+                        if (getModId(current).equals(modid)) {
+                            ignore = true;
+                        }
+                    } catch (NullPointerException e){
+                        e.printStackTrace();
+                    }
+                }
             }
             for (String now : ignoreFiles) {
                 if (current.toString().contains(now.replace("/", "\\"))) {
@@ -156,8 +269,8 @@ public class Downloader extends Task<Void> {
                 }
             }
             if (!ignore) {
-                System.out.println("[IGNORE LIST] Fichier '"+current+"' supprimé");
                 current.delete();
+                System.out.println("[IGNORE LIST] Fichier '"+current+"' supprimé !");
             }
         }
 
@@ -176,7 +289,7 @@ public class Downloader extends Task<Void> {
         //Read file data and update in message digest
         while ((bytesCount = fis.read(byteArray)) != -1) {
             digest.update(byteArray, 0, bytesCount);
-        };
+        }
 
         //close the stream; We don't need it now.
         fis.close();
@@ -201,6 +314,8 @@ public class Downloader extends Task<Void> {
         File cursor;
         toDownload = new JSONArray();
         long temp = System.currentTimeMillis();
+        MessageDigest md5Digest;
+        boolean keep = false;
 
         for (Object array : jsonArray){
             object = (JSONObject) array;
@@ -210,16 +325,31 @@ public class Downloader extends Task<Void> {
                 toDownload.add(object);
                 filesToDownload++;
             } else {
-                if (cursor.length() != Integer.parseInt(object.get("size").toString())){
-                    cursor.delete();
-                    toDownload.add(object);
-                    filesToDownload++;
+                try {
+                    md5Digest = MessageDigest.getInstance("MD5");
+                    keep = false;
+                    for (String now : ignoreFiles){
+
+                        if (now.contains("/")){
+                            now = now.replace("/", "\\");
+                        }
+                        if (cursor.toString().contains(now)){
+                            keep = true;
+                        }
+                    }
+                    if (!getFileChecksum(md5Digest, cursor).equals(object.get("md5")) && !keep){
+                        cursor.delete();
+                        toDownload.add(object);
+                        filesToDownload++;
+                    }
+                } catch (IOException | NoSuchAlgorithmException e) {
+                    e.printStackTrace();
                 }
             }
         }
         System.out.println("[VERIFICATION] temps écoulé vérif: "+(System.currentTimeMillis()-temp));
         System.out.println("[VERIFICATION] Download size: "+GetDownloadSize(toDownload)/1024+"Ko");
-        System.out.println("[VERIFICATION] Files to download: "+filesToDownload);
+        System.out.println("[VERIFICATION] Files to download: "+toDownload);
     }
 
     private void download(File cursor, JSONObject obj){
@@ -304,6 +434,8 @@ public class Downloader extends Task<Void> {
 
     @Override
     protected void succeeded() {
+        vSync();
+        fullScreen();
         System.out.println("[GameUpdater] Downloading time: "+(System.currentTimeMillis()/1000-time)+" sec");
         System.out.println("[GameUpdater] Update finished !");
         super.succeeded();
